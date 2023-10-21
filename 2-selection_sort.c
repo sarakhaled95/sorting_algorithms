c50ff1e3d73d84337b4d6c2a9ab9cfe522f82570
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-21 03:55:30 -0700

    betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,10 +24,10 @@ void selection_sort(int *array, size_t size)
 		}
 		if (min != i)
 		{
-				tmp = array[min];
-				array[min] = array[i];
-				array[i] = tmp;
-				print_array(array, size);
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
 		}
 	}
 }
